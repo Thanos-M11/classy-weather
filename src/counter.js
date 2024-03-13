df4350037dbae42339c4598b1f68133accd00bc7
Thanos-M11 <kalaitzisa@gmail.com>
2024-03-13 21:16:13 +0200

    first commit - React2024 - converted lifecycle methods - lecture 183

---
+++ src/counter.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((cur) => {
+      return {
+        count: cur.count - 1,
+      };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((cur) => {
+      return {
+        count: cur.count + 1,
+      };
+    });
+  }
+
+  render() {
+    const date = new Date("june 20 2024");
+    date.setDate(date.getDate() + this.state.count);
+
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
